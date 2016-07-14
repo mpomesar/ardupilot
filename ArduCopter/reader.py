c2786dc2bb84e242fac417f616e6336d041ce7e8
mpomesar <martipomes@hotmail.com>
2016-07-14 09:30:31 +0200

    Add files via upload

---
+++ ArduCopter/reader.py
@@ -0,0 +1,14 @@
+import serial
+
+def reading_serial():
+    s=serial.Serial(port="/dev/ttyAMA0",baudrate=9600,timeout=0.2)
+    l=s.readline();
+    if s=="":
+        return 0,0,0,0
+    else:
+        line=l.split(';')
+        x=float(line[0])
+        y=float(line[1])
+        z=float(line[2])
+        yaw=float(line[3])
+        return x,y,z,yaw
