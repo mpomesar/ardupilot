c7fd8de0858326fa5d9c7014f458b232ab39c739
mpomesar <martipomes@hotmail.com>
2016-07-15 10:53:38 +0200

    Update read_you_reader.cpp

--- ArduCopter/read_you_reader.cpp
+++ ArduCopter/read_you_reader.cpp
@@ -47,8 +47,8 @@ float dt=0.04;
 _yaw_in=constrain_float(5*(-yaw),-40,40);
 _yaw_in=_yaw_in*45*4.5/40;
 
-_throttle_in=20*(1-z);
-_dz=((1-z)-_inputz)/dt+0.01*(((1-z)-_inputz)/dt-_dz);
+_throttle_in=20*(-z);
+_dz=((-z)-_inputz)/dt+0.01*(((-z)-_inputz)/dt-_dz);
 _throttle_in+=7*_dz;
 _throttle_in=constrain_float(_throttle_in,-25,25);
 _throttle_in=(_throttle_in+25)/50;
