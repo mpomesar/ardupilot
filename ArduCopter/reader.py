743e2ce1b3703e6c3ff3cf087cbc86e4d478666b
mpomesar <martipomes@hotmail.com>
2016-07-15 15:10:50 +0200

    Update reader.py

--- ArduCopter/reader.py
+++ ArduCopter/reader.py
@@ -1,14 +1,14 @@
 import serial
 
 def reading_serial():
-    s=serial.Serial(port="/dev/ttyAMA0",baudrate=9600,timeout=0.1)
+    s=serial.Serial(port="/dev/ttyAMA0",baudrate=9600,timeout=0.0009)
     l=s.readline();
-    if s=="":
-        return 0,0,0,0
+    if l=="":
+        return [0,0,0,0]
     else:
         line=l.split(';')
         x=float(line[0])
         y=float(line[1])
         z=float(line[2])-1
         yaw=float(line[3])
-        return x,y,z,yaw
+        return [x,y,z,yaw]
